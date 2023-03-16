d2f785841f0b27a8667182ab9fb5095ed414e838
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-16 19:00:08 +0600

    event detail

--- pages/events/[eventID].tsx
+++ pages/events/[eventID].tsx
@@ -1,14 +1,14 @@
 import { useRouter } from 'next/router';
 import { getEventById } from '@/dummy-data';
+import ErrorAlert from '@/components/error-alert/error-alert';
 
 function EventDetail() {
     const route = useRouter();
     const eventDetail = getEventById(route.query.eventID as string);
-    return(
-        <>
-            event detail
-        </>
-    )
+    return !eventDetail ? <ErrorAlert /> : <>
+        event detail
+    </>
+
 }
 
 export default EventDetail;
