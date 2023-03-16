6b559b7ca697fa6a8ded085690aba5bab353d8d8
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-16 18:52:34 +0600

    event details

--- pages/events/[eventID].tsx
+++ pages/events/[eventID].tsx
@@ -1,4 +1,9 @@
+import { useRouter } from 'next/router';
+import { getEventById } from '@/dummy-data';
+
 function EventDetail() {
+    const route = useRouter();
+    const eventDetail = getEventById(route.query.eventID as string);
     return(
         <>
             event detail
