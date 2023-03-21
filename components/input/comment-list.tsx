56f70183b3f20080ab3880c00580c9d90b14295a
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-21 16:02:57 +0600

    comment api binding

--- components/input/comment-list.tsx
+++ components/input/comment-list.tsx
@@ -3,7 +3,6 @@ import classes from './comment-list.module.css';
 function CommentList( props: any ) {
     return (
         <ul className={ classes.comments }>
-            {/* Render list of comments - fetched from API */ }
             {
                 props?.comments?.comments.map( ( items: any ) =>
                     <li key={items.id}>
