0de7d0394e1df7ccefd8dac0c7064b1c590758c2
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-22 17:25:34 +0600

    connecting with db

--- pages/api/news-letter.ts
+++ pages/api/news-letter.ts
@@ -1,5 +1,14 @@
 import { MongoClient } from 'mongodb'
-const client = new MongoClient('mongodb+srv://jigmelodey:Psycho0209@cluster0.v5jrt.mongodb.net/?retryWrites=true&w=majority');
+
+async function connectionDatabase() {
+    return new MongoClient( 'mongodb+srv://jigmelodey:Psycho0209@cluster0.v5jrt.mongodb.net/?retryWrites=true&w=majority' )
+}
+
+async function insertDocument(client: any, document: any) {
+    const { email } = document;
+    const db = client.db('news-letter');
+    await db.collection('emails').insertOne( { email })
+}
 
 async function handler( req: any, res: any,  ){
     if ( req.method === 'POST' ) {
@@ -8,9 +17,20 @@ async function handler( req: any, res: any,  ){
             res.status(401).json({ message: 'no email tsk tsk' })
             return
         }
-        const db = client.db('news-letter');
-        await db.collection('emails').insertOne({ email })
-        res.status(201).json({ message: 'successful' });
+        let client;
+        try {
+             client = await connectionDatabase();
+
+        } catch ( err ){
+            res.status(500)
+        }
+        try {
+            await insertDocument(client, email)
+            res.status(201).json({ message: 'successful' });
+
+        }catch ( err ){
+            res.status(500).json( { message: 'tsk tsk failed' })
+        }
         return
     }
     res.status(301).json({ message: 'successful' })
