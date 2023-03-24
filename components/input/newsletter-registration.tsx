fbb1ae97a98875533455107acef3e3b0b54327b5
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-24 14:09:03 +0600

    setting context up

--- components/input/newsletter-registration.tsx
+++ components/input/newsletter-registration.tsx
@@ -1,16 +1,37 @@
 import classes from './newsletter-registration.module.css';
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
+import NotificationContext from '@/store/notification/notification-context';
 
 function NewsletterRegistration() {
 
     const emailRef: any = useRef();
+    const notification = useContext(NotificationContext);
 
     function registrationHandler(event: any) {
+        notification.showNotification({
+            message: 'Registration for news letter.',
+            status: 'pending',
+            title: 'Signing Up'
+        })
         event.preventDefault();
-        fetch('/api/news-letter', { method: 'POST', body: JSON.stringify( { email: emailRef.current.value })}).then((res) => res)
-        // fetch user input (state or refs)
-        // optional: validate input
-        // send valid data to API
+        fetch('/api/news-letter', { method: 'POST', body: JSON.stringify( { email: emailRef.current.value })}).then(
+            (res) =>{
+                if ( res ) {
+                    notification.showNotification({
+                        message: 'Successfully Register',
+                        status: 'success',
+                        title: 'Success'
+                    })
+                }
+            }
+        ).catch((err) => {
+            notification.showNotification({
+                message: 'Sorry try again tsk tsk',
+                status: 'error',
+                title: 'Failed'
+            })
+        })
+
     }
 
     return (
