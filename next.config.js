facc4d8f476edb7707765d2bbac738ebf756170a
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-25 15:37:23 +0600

    setting context up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    mongo_db_username: 'jigmelodey',
+    mongo_db_password: 'Psycho0209',
+
+  }
 }
 
 module.exports = nextConfig
